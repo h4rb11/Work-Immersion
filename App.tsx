c535478e6cf35f229dfb2b2513a342eb3f006595
KimHarveyDiaz <126734526+h4rb11@users.noreply.github.com>
2023-03-29 11:07:57 +0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="myDiv">
+<img src="https://i.ibb.co/s3QJ3Nt/Screenshot-2023-03-17-111803.png" alt="Screenshot-2023-03-17-111803" width="200" height="200" />
+
+    <h1>KIM HARVEY A. DIAZ</h1>
+    <p>Zone 3 Bell Magarao, Camarines Sur</p>
+    <a href="kimharveydiazz@gmail.com">kimharveydiazz@gmail.com</a>
+    <p>09260084949</p>
+
+
+    <p>Specializes in Front-End Developing.</p>
+
+
+    <p className="portions">PERSONAL DATA </p>
+<hr/>
+<p> 
+    <ul>Date of Birth   :   Febuary 15, 2004</ul>
+    <ul>Age             :   19</ul>
+    <ul>Gender          :   Male</ul>
+    <ul>Status          :   Single</ul>
+    <ul>Citizenship     :   Filipino</ul>
+</p>
+
+
+    <p className="portions">SKILLS</p>
+<hr/>
+<p>
+    <ul>
+        <li>HTML Coding</li>
+        <li>Online Research</li>
+        <li>Graphic Design</li>
+        <li>Word Processing</li>
+    </ul>
+</p>
+
+
+    <p className="portions">PROGRAMMING</p>
+<hr/>
+<p> 
+    <ul>
+        <li>HTML</li>
+        <li>JavaScript</li>
+        <li>C++</li>
+    </ul>
+</p>
+
+    <p className="portions">EDUCATION</p>
+<hr/>
+<p>
+    Primary         :   2010 - 2017
+				              Naga Central School 1
+ 
+    Secondary		    :   2017 - 2021
+				              University of Nueva Caceres
+
+    Upper-Secondary	: 	2021 - Present
+				              University of Nueva Caceres
+
+</p>
+
+
+<p className="portions">CERTIFICATION</p>
+    <hr/>
+<p>
+    Febuary 8, 2023	:  	JavaScript Essentials 
+                        Cisco Networking Academy
+</p>
+
+<p className="portions">SEMINARS / TRAINING ATTENDED </p>
+    <hr/>
+<p>
+    Present		: 	Solidity: ETH PROOF: Beginner EVM Course
+                  MetaCrafters
+    
+    
+    Present		: 	Solana: SOL PROOF: Beginner Course
+                  MetaCrafters
+
+</p>    
+
+<p className="portions">ACHIEVEMENT</p>
+    <hr/>
+<p>
+    Sample Works:  <a href="https://drive.google.com/drive/folders/1-dWo4pp0osLMpmARfr_HbWCXMoFuQdgH?usp=sharing">CALCULATOR FOR AVERAGE GRADE WITH LETTER GRADE</a>
+    
+    
+    Febuary 28, 2023	: Game Development 
+				                Title:”Project Anatomy” 
+				                Language:Construct 3 
+				                Role:Researcher, Debugger
+                        EXE FILE: <a href="https://drive.google.com/drive/folders/1X3Jutrh-81a0ccY17ezFvtplX-ZEoNUU?usp=sharing">Project Anatomy</a>
+</p>
+
+<p className="portions">REFERENCE</p>
+    <hr/>
+<p>
+    Nobelle V. Felizardo-Zantua
+    09504603870
+    <a href="nobelle.felizardo@unc.edu.ph">nobelle.felizardo@unc.edu.ph</a>
+    
+    
+    Jona Christine P. De Jesus 
+    09385177976 
+    <a href="jonachristine.dejesus@unc.edu.ph">jonachristine.dejesus@unc.edu.ph</a>
+    
+    
+</p>
+</div>
+
+</div>
+ 
+)
+}
+
+export default App;
